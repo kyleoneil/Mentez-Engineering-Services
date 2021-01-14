a2b749ef78f37da54837e3119423865ee2834c32
MinatoSana <carljamesbalingit@gmail.com>
2021-01-14 20:50:44 +0800

    start

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
+const methodOver = require('method-override');
+const bcrypt = require('bcrypt');
+const connection= mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root", 
+    password :  "",
+    database: 'schoolest'
+});
+connection.connect((err)=>{
+    if(err) throw err;
+    console.log("Database Connected!");
+});
+const app = express();
+app.use(bodyParser.json());
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOver("_method"));
+
+app.get('/',(req,res)=>{
+    res.send("welcome")
+})
