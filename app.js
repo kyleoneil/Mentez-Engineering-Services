c7ae3988e796ce458d805733335be8b45761bb96
MinatoSana <carljamesbalingit@gmail.com>
2021-01-14 21:16:20 +0800

    db connected

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const connection= mysql.createConnection({
     host: "127.0.0.1",
     user: "root", 
     password :  "",
-    database: 'schoolest'
+    database: 'mendezengineering'
 });
 connection.connect((err)=>{
     if(err) throw err;
