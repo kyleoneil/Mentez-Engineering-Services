d4ab157cb18c8411476c614ec8a5018066e890b7
kyleoneil <kyleoneil05@gmail.com>
2021-01-15 19:52:06 +0800

    k: Partially finished Login

--- app.js
+++ app.js
@@ -5,12 +5,17 @@ const mysql = require('mysql');
 const cors = require('cors');
 const methodOver = require('method-override');
 const bcrypt = require('bcrypt');
+const saltR = 10;
+const empty = require("is-empty");
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+
 const connection= mysql.createConnection({
     host: "127.0.0.1",
     user: "root", 
     password :  "",
     database: 'mendezengineering'
 });
+
 connection.connect((err)=>{
     if(err) throw err;
     console.log("Database Connected!");
@@ -21,13 +26,40 @@ app.use(cors())
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOver("_method"));
 
+app.use(session({
+    secret: "Ch43y0Vn6Num84W4N",
+    saveUninitialized: true,
+    resave: true
+}));
 
-app.get('/',(req,res)=>{
+app.post('/login/user',urlencodedParser,(req,res)=>{
     var xd;
-    connection.query("SELECT * FROM users", (err, response) => {
-       xd = response;
-       console.log(xd);
-       res.send(xd);
+    let user = req.body.username;
+    let pass = req.body.password;
+   
+   
+    let salt = bcrypt.genSaltSync(saltR);
+    let hash = bcrypt.hashSync(pass,salt);
+
+    connection.query('SELECT username FROM users WHERE username ="'+user+'"', (err, result)=> {
+    
+	if(empty(result)){
+            res.send({message:"Account does not exist"});
+        }else{
+            connection.query('SELECT Username, Password,UserID FROM users WHERE Username ="'+user+'"',(err,result)=>{       
+            
+                let check = bcrypt.compareSync(pass,result[0]['Password']);      
+                if(check){
+                    req.session.loggedIn = true;
+                    res.send({id:result[0]['UserID']});
+                }else if(pass==result[0]['Password']){
+                    req.session.loggedIn = true;
+                    res.send({id:result[0]['UserID']});
+                }else{ 
+                    res.send({message:"Incorrect Password"});
+                }
+            })
+        }
     })
     
 })
